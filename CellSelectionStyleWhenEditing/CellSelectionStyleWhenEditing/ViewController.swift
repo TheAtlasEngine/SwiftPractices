4788c74a6eeef87779b0e5216a7fef799e3ebb9f
Kosuke Nishimura <blackurocky@gmail.com>
2019-05-24 22:02:23 +0900

    initial commit

---
+++ CellSelectionStyleWhenEditing/CellSelectionStyleWhenEditing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CellSelectionStyleWhenEditing
+//
+//  Created by Kosuke Nishimura on 2019/05/24.
+//  Copyright © 2019 Kosuke.Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
