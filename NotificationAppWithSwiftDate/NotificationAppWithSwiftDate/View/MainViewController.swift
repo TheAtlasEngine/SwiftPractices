0270922a9572befd418d1145741f8d30de164096
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 11:48:16 +0900

    Add MainViewController

--- NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/MainViewController.swift
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NotificationAppWithSwiftDate
 //
 //  Created by Kosuke Nishimura on 2019/10/12.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
