03319abe022758f915a18c3af756291628054a75
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 11:24:56 +0900

    Define RepeatUnit

---
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/Model/RepeatUnit.swift
@@ -0,0 +1,15 @@
+//
+//  RepeatUnit.swift
+//  NotificationAppWithSwiftDate
+//
+//  Created by Kosuke Nishimura on 2019/10/12.
+//  Copyright © 2019 Kosuke.Nishimura. All rights reserved.
+//
+
+import Foundation
+
+enum RepeatUnit: String {
+    case everyday
+    case everyWeek
+    case everyMonth
+}
