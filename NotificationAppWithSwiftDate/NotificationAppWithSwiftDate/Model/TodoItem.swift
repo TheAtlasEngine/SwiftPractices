eb287dd35b1eeb90f83fa9110013a8f360544454
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 11:17:50 +0900

    Add Unit Test Module and Model Object

---
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/Model/TodoItem.swift
@@ -0,0 +1,9 @@
+//
+//  TodoItem.swift
+//  NotificationAppWithSwiftDate
+//
+//  Created by Kosuke Nishimura on 2019/10/12.
+//  Copyright © 2019 Kosuke.Nishimura. All rights reserved.
+//
+
+import Foundation
