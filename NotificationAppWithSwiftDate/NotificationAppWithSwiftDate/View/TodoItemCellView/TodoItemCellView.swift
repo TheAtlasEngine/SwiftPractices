2107bad87346a2262bd97550ef7f05ef33976492
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 12:23:17 +0900

    add new group and  files: TodoItemCellView

---
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/TodoItemCellView/TodoItemCellView.swift
@@ -0,0 +1,24 @@
+//
+//  TodoItemCellView.swift
+//  NotificationAppWithSwiftDate
+//
+//  Created by Kosuke Nishimura on 2019/10/12.
+//  Copyright © 2019 Kosuke.Nishimura. All rights reserved.
+//
+
+import UIKit
+
+final class TodoItemCellView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
