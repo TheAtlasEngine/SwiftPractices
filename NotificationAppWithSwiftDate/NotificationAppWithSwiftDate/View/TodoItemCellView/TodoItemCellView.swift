3c593dd9fc072f1505a286c3bde49a8ca95aa539
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 12:44:19 +0900

    implemented biding method

--- NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/TodoItemCellView/TodoItemCellView.swift
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/TodoItemCellView/TodoItemCellView.swift
@@ -12,17 +12,24 @@ final class TodoItemCellView: UITableViewCell {
 
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var deadLineLabel: UILabel!
-    @IBOutlet private weak var RepeatUnitLabel: UILabel!
+    @IBOutlet private weak var repeatUnitLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func bind(_ viewModel: TodoItemCellViewModel) {
+        
+        titleLabel.text = viewModel.title
+        repeatUnitLabel.text = viewModel.repeatUnit.rawValue
+        
+        let deadLine = viewModel.deadLine
+        if let year = deadLine.year, let month = deadLine.month, let day = deadLine.day {
+            deadLineLabel.text = "year: \(year) month: \(month) day: \(day)"
+        } else {
+            repeatUnitLabel.text = ""
+        }
     }
     
 }
