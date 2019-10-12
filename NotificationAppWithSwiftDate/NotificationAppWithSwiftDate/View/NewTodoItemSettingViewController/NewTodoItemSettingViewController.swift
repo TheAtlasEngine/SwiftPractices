82e90203a4831b8c2c80ede734832c509d0b0c8a
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-12 13:14:30 +0900

    NewTodoItemSettingViewController: add new files

---
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/NewTodoItemSettingViewController/NewTodoItemSettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewTodoItemSettingViewController.swift
+//  NotificationAppWithSwiftDate
+//
+//  Created by Kosuke Nishimura on 2019/10/12.
+//  Copyright © 2019 Kosuke.Nishimura. All rights reserved.
+//
+
+import UIKit
+
+final class NewTodoItemSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
