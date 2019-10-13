d093543ce4ccdbed5572ec2aaa3d116c2d1e9761
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-13 14:56:49 +0900

    implemented a new method

--- NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/NewTodoItemSettingViewController/NewTodoItemSettingViewController.swift
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/View/NewTodoItemSettingViewController/NewTodoItemSettingViewController.swift
@@ -15,6 +15,14 @@ final class NewTodoItemSettingViewController: UIViewController {
     @IBOutlet private weak var monthPickerTextField: PickerTextField!
     @IBOutlet private weak var dayPickerTextField: PickerTextField!
     
+    private var viewModel: NewTodoItemSettingViewModel!
+    
+    enum Event {
+        case add(TodoItem)
+    }
+    
+    var onEvent: ((Event) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,12 +33,35 @@ final class NewTodoItemSettingViewController: UIViewController {
         view.endEditing(true)
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func addNewTodoItem(_ sender: Any) {
+        view.endEditing(true)
+        
+        guard let title = titleTextField.text else { return }
+        guard let yearString = yearPickerTextField.text else { return }
+        guard let monthString = monthPickerTextField.text else { return }
+        guard let dayString = dayPickerTextField.text else { return }
+        
+        guard let year = Int(yearString) else { return }
+        guard let month = Int(monthString) else { return }
+        guard let day = Int(dayString) else { return }
+        
+        guard !title.isEmpty else { return }
+        
+        viewModel.setTitle(title)
+        viewModel.setDeadLine(year: year, month: month, day: day)
+        viewModel.setRepeatUnit(.none)
+        
+        let todoItem = viewModel.makeNewTodoItem()
+        onEvent?(.add(todoItem))
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 private extension NewTodoItemSettingViewController {
     
     func bind() {
-        let viewModel = DefaultNewTodoItemSettingViewModel()
+        viewModel = DefaultNewTodoItemSettingViewModel()
         
         yearPickerTextField.setDataList(viewModel.yearList)
         monthPickerTextField.setDataList(viewModel.monthList)
