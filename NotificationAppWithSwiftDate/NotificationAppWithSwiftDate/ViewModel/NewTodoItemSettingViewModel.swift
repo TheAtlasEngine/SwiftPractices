bf205e9b16bd602bdc531b9454c5e97dff0bd88f
Kosuke Nishimura <blackurocky@gmail.com>
2019-10-13 13:48:44 +0900

    set data list  for picker view

--- NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/ViewModel/NewTodoItemSettingViewModel.swift
+++ NotificationAppWithSwiftDate/NotificationAppWithSwiftDate/ViewModel/NewTodoItemSettingViewModel.swift
@@ -7,12 +7,17 @@
 //
 
 import Foundation
+import SwiftDate
 
 protocol NewTodoItemSettingViewModel {
     
     var title: String { get }
     var deadLine: DateComponents { get }
     var repeatUnit: RepeatUnit { get }
+    
+    var yearList: [String] { get }
+    var monthList: [String] { get }
+    var dayList: [String] { get }
     
     mutating func setTitle(_ title: String)
     mutating func setDeadLine(year: Int, month: Int, day: Int)
@@ -26,6 +31,24 @@ struct DefaultNewTodoItemSettingViewModel: NewTodoItemSettingViewModel {
     private(set) var title: String
     private(set) var deadLine: DateComponents
     private(set) var repeatUnit: RepeatUnit
+    
+    private(set) var yearList: [String]
+    private(set) var monthList: [String]
+    private(set) var dayList: [String]
+    
+    init() {
+        self.title = ""
+        self.deadLine = .init()
+        self.repeatUnit = .none
+        
+        let japan = Region(calendar: Calendars.gregorian, zone: Zones.asiaTokyo, locale: Locales.japanese)
+        SwiftDate.defaultRegion = japan
+        let today = DateInRegion(Date(), region: japan)
+        let thisYear = today.year
+        self.yearList = (thisYear...(thisYear + 10)).map { "\($0)" }
+        self.monthList = (1...12).map { "\($0)"}
+        self.dayList = (1...today.monthDays).map { "\($0)" }
+    }
     
     mutating func setTitle(_ title: String) {
         self.title = title
